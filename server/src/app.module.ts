007f9579c23fe427e84e3d8aea01935ce3dc22e1
Jean <jeanc16rlos@gmail.com>
2023-07-18 12:28:37 +0300

    feat(db): create db config
    Create Initial Sqlite and Sequelize config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      storage: './database.sqlite',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
