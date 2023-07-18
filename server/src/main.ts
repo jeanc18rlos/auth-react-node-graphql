5701fcba0d61bcc2a80590aeb6559cf7e15a8e44
Jean <jeanc16rlos@gmail.com>
2023-07-18 12:25:12 +0300

    feat(config): create env vars
    Create Env Vars config

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.enableCors({
+    origin: process.env.CLIENT_ALLOWED_ORIGIN,
+  });
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
