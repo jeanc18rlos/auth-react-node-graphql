64498c784165b7cb1961699d9b72d235cded6703
Jean <jeanc16rlos@gmail.com>
2023-07-20 15:30:25 +0300

    feat(auth): add actions to auth service
    create login, register, requestPasswordChange and confirmPasswordChange actions

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {
   Inject,
   Injectable,
+  NotFoundException,
   UnauthorizedException,
   forwardRef,
 } from '@nestjs/common';
@@ -34,7 +35,63 @@ export class AuthService {
 
     return user;
   }
+  async login(email: string, password: string) {
+    const user = await this.validateUser(email, password);
+    if (user) {
+      const payload = { email: user.email, id: user.id };
+      return {
+        email,
+        id: user.id,
+        access_token: this.jwtService.sign(payload),
+      };
+    }
+  }
 
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userModel.create({
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      return user;
+    }
+  }
+
+  async requestPasswordChange(email: string): Promise<{
+    password_change_token: string;
+  }> {
+    const user = await this.userModel.findOne({ where: { email } });
+    if (user) {
+      const payload = { email: user.email };
+      return {
+        password_change_token: this.jwtService.sign(payload),
+      };
+    } else {
+      throw new NotFoundException();
+    }
+  }
+
+  async confirmPasswordChange(
+    email: string,
+    token: string,
+    newPassword: string,
+  ) {
+    try {
+      const payload = this.jwtService.verify(token);
+      if (payload.email === email) {
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        await this.userModel.update(
+          { password: hashedPassword },
+          { where: { email } },
+        );
+        return true;
+      }
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
   async validateJwtPayload(payload: JwtPayload): Promise<User | undefined> {
     const user = await this.userService.findOne(payload.email);
 
